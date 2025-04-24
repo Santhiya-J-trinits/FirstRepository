bdd6ec3e9d1edfd6a41b50893dde6968cdecbd0f
Santhiya-J-trinits <santhiya.jeyabal@trinitstechnologies.com>
2025-04-24 18:22:43 +0530

    This another text file

--- src/HelloClass.java
+++ src/HelloClass.java
@@ -2,5 +2,6 @@ public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Hello Intellij IDEA");
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
